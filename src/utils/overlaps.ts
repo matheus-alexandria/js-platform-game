af55d55e453afd471d4ee406df3a85926c8741be
Matchobas <maalexandria@hotmail.com>
2024-05-02 23:24:09 -0300

    feat: create overlaps method to check collision

---
+++ src/utils/overlaps.ts
@@ -0,0 +1,10 @@
+import { Actor } from "../Actor";
+
+export function overlaps(actor1: Actor, actor2: Actor) {
+  return (
+    actor1.pos.x + actor1.size.x > actor2.pos.x &&
+    actor1.pos.x < actor2.pos.x + actor2.size.x &&
+    actor1.pos.y + actor1.size.y > actor2.pos.y &&
+    actor1.pos.y < actor2.pos.y + actor2.size.y
+  )
+}
