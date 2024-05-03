cc91de88c38975e7fdc6a3de57d2c8ba894df765
Matchobas <maalexandria@hotmail.com>
2024-05-02 23:24:49 -0300

    feat: create touches method to check if a object is touching other

--- src/Level.ts
+++ src/Level.ts
@@ -3,10 +3,10 @@ import { Vec } from "./Vec";
 import { levelChars } from "./utils/levelChars";
 
 export class Level {
-  public height;
-  public width;
+  public height: number;
+  public width: number;
   public startActors: Actor[];
-  public rows;
+  public rows: string[][];
 
   constructor(plan: string) {
     let rows = plan.trim().split("\n").map(l => [...l]);
@@ -25,5 +25,26 @@ export class Level {
         return "empty";
       });
     });
+  }
+
+  touches(pos: Vec, size: Vec, type: string): boolean {
+    const xStart = Math.floor(pos.x);
+    const xEnd = Math.ceil(pos.x + size.x);
+    const yStart = Math.floor(pos.y);
+    const yEnd = Math.ceil(pos.y + size.y);
+
+    const isOutside = xStart < 0 || xEnd < this.width || yStart < 0 || yEnd > this.height;
+    const squaresTypes = [
+      this.rows[yStart][xStart], 
+      this.rows[yStart][xEnd], 
+      this.rows[yEnd][xStart], 
+      this.rows[yEnd][yEnd]
+    ];
+    
+    const here = isOutside ? ['wall'] : squaresTypes;
+
+    if (here.includes(type)) return true;
+
+    return false;
   }
 }
