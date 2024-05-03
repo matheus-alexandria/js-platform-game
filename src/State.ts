5fcf38d372785a1203f602b742bb1a597a6c5f74
Matchobas <maalexandria@hotmail.com>
2024-05-02 23:25:01 -0300

    feat: state update method incomplete

--- src/State.ts
+++ src/State.ts
@@ -1,5 +1,6 @@
 import { Actor } from "./Actor";
 import { Level } from "./Level";
+import { overlaps } from "./utils/overlaps";
 
 export class State {
   public level: Level;
@@ -18,6 +19,29 @@ export class State {
 
   get player() {
     return this.actors.find(a => a.type == "player");
+  }
+
+  update(time, keys): State {
+    const actors = this.actors.map((actor) => actor.update(time, this, keys));
+
+    let newState = new State({ level: this.level, actors, status: this.status });
+    if (newState.status !== 'playing') return newState;
+
+    let player = newState.player!;
+    if (
+      player?.pos && player.size &&
+      this.level.touches(player.pos, player.size, 'lava')
+    ) {
+      return new State({ level: this.level, actors, status: 'lost'});
+    }
+
+    for (let actor of actors) {
+      if (actor.type !== 'player' && overlaps(player, actor)) {
+        newState = actor.collides(newState);
+      }
+    }
+
+    return newState;
   }
 }
 
