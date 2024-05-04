40fed798109dc79db312ad50e25e4ecbce753a61
Matchobas <maalexandria@hotmail.com>
2024-05-03 23:22:36 -0300

    feat: create update method for player and speed constants

--- src/Player.ts
+++ src/Player.ts
@@ -1,4 +1,9 @@
+import { State } from "./State";
 import { Vec } from "./Vec";
+
+const gravity = 30;
+const playerXSpeed = 7;
+const jumpSpeed = 14;
 
 export class Player {
   public pos: Vec;
@@ -14,5 +19,29 @@ export class Player {
 
   static create(pos: Vec) {
     return new Player(pos.plus(new Vec(0, -0.5)), new Vec(0, 0));
+  }
+
+  update(time: number, state: State, keys: Record<string, boolean>): Player {
+    let xSpeed = 0;
+    if (keys.ArrowLeft) xSpeed -= playerXSpeed;
+    if (keys.ArrowRight) xSpeed += playerXSpeed;
+
+    let pos = this.pos;
+    let xMoved = pos.plus(new Vec(xSpeed * time, 0));
+    if (!state.level.touches(xMoved, this.size, 'wall')) {
+      pos = xMoved;
+    }
+
+    let ySpeed = this.speed.y + time * gravity;
+    let yMoved = pos.plus(new Vec(0, ySpeed * time));
+    if (!state.level.touches(yMoved, this.size, 'wall')) {
+      pos = yMoved;
+    } else if (keys.ArrowUp && ySpeed > 0) {
+      ySpeed = -jumpSpeed;
+    } else {
+      ySpeed = 0;
+    }
+
+    return new Player(pos, new Vec(xSpeed, ySpeed));
   }
 }
