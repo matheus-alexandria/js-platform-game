e6c1c492b70a55fd8a8a64726eb0c15fc780f4a5
Matchobas <maalexandria@hotmail.com>
2024-05-04 20:28:39 -0300

    feat: create method to clean events

--- src/utils/trackKeys.ts
+++ src/utils/trackKeys.ts
@@ -1,15 +1,23 @@
-export function trackKeys(keys: string[]): Record<string, boolean> {
+export function trackKeys(keys: string[], endEvent = false): TrackReturn {
   const downKeys = Object.create(null);
   function track(event: KeyboardEvent) {
     if (keys.includes(event.key)) {
-      console.log(event.key);
       downKeys[event.key] = event.type === 'keydown';
       event.preventDefault();
     }
+  }
+  function cleanEvents() {
+    window.removeEventListener('keydown', track);
+    window.removeEventListener('keyup', track);  
   }
 
   window.addEventListener('keydown', track);
   window.addEventListener('keyup', track);
 
-  return downKeys;
+  return { keys: downKeys, clean: cleanEvents };
+}
+
+export type TrackReturn = {
+  keys: Record<string, boolean>;
+  clean: () => void;
 }
